1384bf29347e95db10f5c4664f2db5a20b077a63
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-09-22 18:14:28 -0500

    Update llama2.py

--- llama2.py
+++ llama2.py
@@ -10,15 +10,15 @@ st.set_page_config(page_title="Llama2🦙🤖 ")
 page = st.sidebar.selectbox("Seleccione una función", ("Chat", "Resumen"))
 with st.sidebar:
     if 'REPLICATE_API_TOKEN' in st.secrets:
-            st.success('API key already provided!', icon='✅')
-            replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            replicate_api = st.text_input('Enter Replicate API token:', type='password')
-            if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-                st.warning('Please enter your credentials!', icon='⚠️')
-            else:
-                st.success('Proceed to entering your prompt message!', icon='👉')
-        os.environ['REPLICATE_API_TOKEN'] = replicate_api
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 
 if page == "Chat":
