b35b6a80c94808f8bedafc3b9f9ad3443725a1ae
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-12-17 22:42:50 -0500

    Update llama2.py

--- llama2.py
+++ llama2.py
@@ -55,7 +55,7 @@ if page == "Chat":
             llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
         temperature = st.sidebar.slider('creatividad del modelo', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
         top_p = st.sidebar.slider('creatividad de palabras', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-        max_length = st.sidebar.slider('longitud de respuestas', min_value=32, max_value=500, value=120, step=8)
+        max_length = st.sidebar.slider('longitud de respuestas', min_value=32, max_value=600, value=420, step=8)
     
     
     # Store LLM generated responses
